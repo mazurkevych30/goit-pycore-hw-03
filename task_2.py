d17cb6cf04ef125473bdcabd6c680ab222ffc4dc
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2024-12-10 18:19:51 +0000

    fixed task_2

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int] | str:
     if min < 1 or max > 1000:
         return "Мінімальне значення повинно бути більши 0, а максимальне менше 1000."
-    elif min > max or max < quantity:
+    elif min > max or max-min < quantity:
         return f"Мінімальне значення більше максимального. Або недостатній діапазон чисел для списку з {quantity} унікальних чисел."
 
     numbers = set()
@@ -15,4 +15,4 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int] | str:
     return numbers
 
 
-print(get_numbers_ticket(1, 49, 6))
+print(get_numbers_ticket(10, 11, 6))
